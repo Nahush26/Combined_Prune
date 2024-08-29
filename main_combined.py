1d6011d83d20ee20dac648c1d94bb46d088a5532
Ubuntu <azureuser@shwu-t4.ogd5db3d5bwujlacqlz5jvbsxc.bx.internal.cloudapp.net>
2024-08-29 10:00:43 +0000

    Added support for combined pruning

--- main_combined.py
+++ main_combined.py
@@ -2,22 +2,18 @@ import argparse
 import os 
 import numpy as np
 import torch
+import gc
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from models.hf_llama.modeling_llama import LlamaForCausalLM
-
+from datasets import load_dataset
 from importlib.metadata import version
 import os
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
-from lib.prune import prune_flap
+from lib.prune import prune_flap, calculate_bi, prune_model_blocks
 from lib.eval import eval_ppl
 import lm_eval
 from lm_eval.models.huggingface import HFLM
 import sys
-
-print('torch', version('torch'))
-print('transformers', version('transformers'))
-print('accelerate', version('accelerate'))
-print('# of gpus: ', torch.cuda.device_count())
 
 def get_llm(model, device):
     if device=='auto':
@@ -42,7 +38,6 @@ def get_llm(model, device):
         
     model.seqlen = 128
     return model
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default="meta-llama/Meta-Llama-3-8B", help='LLaMA model')    # Huggingface model name
@@ -50,6 +45,10 @@ def main():
     parser.add_argument('--nsamples', type=int, default=1024, help='Number of calibration samples.')
     parser.add_argument('--pruning_ratio', type=float, default=0.5, help='Pruning ratio.')
     parser.add_argument('--remove_heads', type=int, default=-1, help='Remove num_heads')
+    parser.add_argument('--num_blocks_to_prune', type=int, default=3, help='Remove num blocks')
+    parser.add_argument('--pruning_method', type=str, default="cosine_similarity", help='block pruning method')
+    parser.add_argument('--pruning_token', type=str, default="all")
+    parser.add_argument('--calculate_ppl', type=bool, default=True)
     parser.add_argument("--metrics", type=str, default="WIFV", choices=["IFV", "WIFV", "WIFN", 'N/A'])
     parser.add_argument("--structure", type=str, default="AL-AM", choices=["AL-AM"])
     parser.add_argument("--prune_method", type=str, default="flap", choices=["flap"])
@@ -57,110 +56,50 @@ def main():
     parser.add_argument('--eval', action="store_true")
     parser.add_argument('--save_model', type=str, default=None, help='Path to save the pruned model.')
     parser.add_argument('--device', type=str, default='0')
+    parser.add_argument('--block_pruning_bs', type=int, default=2)
     parser.add_argument('--gqa_groups', type=int, default=7, help='Number of gqa groups, 1 for no GQA.')
     parser.add_argument('--start_pruning_layer_idx', type=int, default=18, help='Layer idx post which pruning starts')
     parser.add_argument('--head_dim', type=int, default=64)
     parser.add_argument('--hidden_dim', type=int, default=896)
+    parser.add_argument('--skip_blocks', type=list, default=[1])
+    parser.add_argument('--block_first', type=bool, default=-True)
+    parser.add_argument('--perform_eval', type=bool, default=-True)
     args = parser.parse_args()
-    
-    # Setting seeds for reproducibility
+
     np.random.seed(args.seed)
     torch.random.manual_seed(args.seed)
-
-    # Build the model and tokenizer
-    print(f"loading llm model {args.model}")
-    model = get_llm(args.model, args.device)
-    model.eval()
-    print(f"model parameter {sum(p.numel() for p in model.parameters()) / 1000 ** 3:.2f}B")
-    tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=False)
-
     if args.device=='auto':
         device = model.hf_device_map["lm_head"]
     else:
         device = torch.device(f"cuda:{args.device}")
+    # Build the model and tokenizer
+    print(f"loading llm model {args.model}")
+    model = get_llm(args.model, args.device)
+    dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="test").filter(lambda example: len(example["text"].split())>100).select(list(range(100))) # 100 samples for pruning metric computation
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.block_pruning_bs, shuffle=True)
+    tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=False)
+    print(f"Unpruned model parameters {sum(p.numel() for p in model.parameters()) / 1000 ** 3:.2f}B")
 
-    # Prune the model
-    # print(model.model.num_heads, model.model.head_dim)
-    print("pruning starts")
-    if args.prune_method == "flap":
-        if args.metrics == 'N/A':
-            raise ValueError("For FLAP pruning, the metrics parameter must be chosen from ['IFV', 'WIFV', 'WIFN']. 'N/A' is not a valid choice.")  
-        if args.structure == 'N/A':
-            raise ValueError("For FLAP pruning, the compressed model structure parameter must be chosen from ['UL-UM', 'UL-MM', 'AL-MM', 'AL-AM']. 'N/A' is not a valid choice.")  
+    if args.block_first:
+        print(f"Performing Block Pruning followed by FLAP")
+        bi_scores = calculate_bi(model, dataloader, tokenizer, args.pruning_method, args.pruning_token)
+        block_pruned_model = prune_model_blocks(model, bi_scores, args.num_blocks_to_prune, args.skip_blocks)
+        del model
+        torch.cuda.empty_cache()
+        gc.collect()
+        block_pruned_model.to(device)
+        prune_flap(args, block_pruned_model, tokenizer, device)
+        print(f"Pruned model parameter {sum(p.numel() for p in block_pruned_model.parameters()) / 1000 ** 3:.2f}B")
+
+    else:
+        print("Performing FLAP followed by Block Pruning")
         prune_flap(args, model, tokenizer, device)
-
-    # Check the sparsity of the model
-    print("*"*30)
-    print(f"model parameter {sum(p.numel() for p in model.parameters()) / 1000 ** 3:.2f}B")
-    print("*"*30)
-    # print(model.model.num_heads, model.model.head_dim)
-    # Save the model
-    # if args.save_model:
-    #     if not os.path.exists(args.save_model):
-    #         os.makedirs(args.save_model)
-    #     # torch.save(model, f'{args.save_model}/pruned_model.pt')
-    #     model.save_pretrained(args.save_model)
-    #     tokenizer.save_pretrained(args.save_model)
-
-    # Evaluate the model
-    if args.eval:
-        lm_obj = HFLM(pretrained=model)
-        task_manager = lm_eval.tasks.TaskManager()
-        results = []
-        result = lm_eval.simple_evaluate(
-            model=lm_obj,
-            tasks=["winogrande"],
-            num_fewshot=5,
-            task_manager=task_manager,
-            batch_size='auto',
-        )
-        results.append(result['results'])
-        print(results)
-        result = lm_eval.simple_evaluate(
-            model=lm_obj,
-            tasks=["gsm8k"],
-            num_fewshot=5,
-            task_manager=task_manager,
-            batch_size='auto',
-        )
-        results.append(result['results'])
-        print(results)
-        result = lm_eval.simple_evaluate(
-            model=lm_obj,
-            tasks=["mmlu"],
-            num_fewshot=5,
-            task_manager=task_manager,
-            batch_size='auto',
-        )
-        results.append(result['results'])
-        print(results)
-        result = lm_eval.simple_evaluate(
-            model=lm_obj,
-            tasks=["boolq"],
-            num_fewshot=0,
-            task_manager=task_manager,
-            batch_size='auto',
-        )
-        results.append(result['results'])
-        print(results)
-        result = lm_eval.simple_evaluate(
-            model=lm_obj,
-            tasks=["arc_challenge"],
-            num_fewshot=25,
-            task_manager=task_manager,
-            batch_size='auto',
-        )
-        results.append(result['results'])
-        print(results)
-        result = lm_eval.simple_evaluate(
-            model=lm_obj,
-            tasks=["hellaswag"],
-            num_fewshot=10,
-            task_manager=task_manager,
-            batch_size='auto',
-        )
-        results.append(result['results'])
-        print(results)
+        bi_scores = calculate_bi(model, dataloader, tokenizer, args.pruning_method, args.pruning_token)
+        block_pruned_model = prune_model_blocks(model, bi_scores, args.num_blocks_to_prune, args.skip_blocks)
+        del model
+        torch.cuda.empty_cache()
+        gc.collect()
+        block_pruned_model.to(device)
 
 if __name__ == '__main__':
     main()
